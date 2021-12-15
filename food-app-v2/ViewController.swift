79b718afd75d804db463b785fd1f5e07d43cf428
Ted <jarod_ph2001@yahoo.com>
2021-12-15 15:01:27 +0800

    this is the start of the commit

---
+++ food-app-v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  food-app-v2
+//
+//  Created by Ted Antipolo on 12/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
